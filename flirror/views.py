6b7b1637408d0a50532a61599e0918ba4120f46f
Felix Schmidt <felixfelix.schmidt@googlemail.com>
2019-05-11 22:12:49 +0200

    Retrieve weather data from openweathermap

--- flirror/views.py
+++ flirror/views.py
@@ -1,7 +1,10 @@
 import abc
+import functools
+from datetime import datetime
 
 from flask import current_app, render_template
 from flask.views import MethodView
+from pyowm import OWM
 
 
 class FlirrorMethodView(MethodView):
@@ -54,13 +57,45 @@ class WeatherView(FlirrorMethodView):
     def get(self):
         # Get view-specific settings from config
         settings = current_app.config["MODULES"].get(self.endpoint)
-        context = self.get_context(**settings)
+        weather_data = self.get_weather(settings)
+        context = self.get_context(**weather_data)
         return render_template(self.template_name, **context)
+
+    def get_weather(self, settings):
+        api_key = settings.get("api_key")
+        language = settings.get("language")
+        town = settings.get("town")
+        temp_unit = settings.get("temp_unit")
+
+        owm = OWM(API_key=api_key, language=language, version="2.5")
+        obs = owm.weather_at_place(town)
+        weather = obs.get_weather()
+
+        fc = owm.daily_forecast(town, limit=6)
+
+        fc_data = []
+        for fc_weather in fc.get_forecast():
+            fc_data.append({
+                "date": fc_weather.get_reference_time(timeformat="date"),
+                "temperature": fc_weather.get_temperature(unit=temp_unit),
+                "status": fc_weather.get_status(),
+            })
+
+        return {
+            "town": town,
+            "date": weather.get_reference_time(timeformat="date"),
+            "temperature": weather.get_temperature(unit=temp_unit),
+            "status": weather.get_status(),
+            "detailed_status": weather.get_detailed_status(),
+            "sunrise_time": weather.get_sunrise_time("iso"),
+            "sunset_time": weather.get_sunset_time("iso"),
+            "forecast": fc_data,
+        }
 
 
 class CalendarView(FlirrorMethodView):
 
-    endpoint ="calendar"
+    endpoint = "calendar"
     rule = "/calendar"
     template_name = "calendar.html"
 
