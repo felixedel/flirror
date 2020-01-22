cd6521c9043d90cde69c03345203f8999f52e380
Felix Edel <felixfelix.schmidt@googlemail.com>
2020-01-22 22:23:54 +0100

    Create a flask test client that raises exceptions on undefined variables
    We can use this to validate the template rendering in unit tests and
    ensure that the data structure stored in the database is sufficient to
    render those templates properly.

--- flirror/__init__.py
+++ flirror/__init__.py
@@ -9,9 +9,22 @@ from .views import CalendarApi, IndexView, NewsfeedApi, StocksApi, WeatherApi
 FLIRROR_SETTINGS_ENV = "FLIRROR_SETTINGS"
 
 
-def create_app():
+def create_app(config=None, jinja_options=None):
+    # TODO (felix): Find a better way to overwrite the jinja_options for the unit tests.
+    # As stated in https://github.com/pallets/flask/blob/38eb5d3b49d628785a470e2e773fc5ac82e3c8e4/src/flask/app.py#L679
+    # overwriting the jinja_options should be done as early as possible.
     app = Flask(__name__)
+
+    # Overwrite or set additional jinja_options. This is currently only used for
+    # validating the templates in the unit tests.
+    if jinja_options is not None:
+        app.jinja_options = {**app.jinja_options, **jinja_options}
+
+    # Load the config from the python file specified in the env vars and
+    # overwrite values that are provided directly via arguments.
     app.config.from_envvar(FLIRROR_SETTINGS_ENV)
+    if config is not None:
+        app.config.from_mapping(config)
 
     app.secret_key = app.config["SECRET_KEY"]
 
@@ -47,7 +60,7 @@ def create_app():
     # Usually, it should be sufficient, when the crawler creates the database. If it
     # is not created here, we should just provide somee message to start the crawler.
     db = create_database_and_entities(
-        provider="sqlite", filename=app.config["DATABASE_FILE"], create_db=True
+        provider="sqlite", filename=app.config["DATABASE_FILE"], create_db=True,
     )
 
     # Store the dabase connection in flask's extensions dictionary.
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import os
 
 import pytest
+from jinja2 import Undefined
+from jinja2.exceptions import UndefinedError
 
+from flirror import create_app
 from flirror.database import create_database_and_entities
 
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testdata")
@@ -22,3 +25,28 @@ def mock_empty_database(tmpdir):
     )
     yield db
     db.disconnect()
+
+
+class ExceptionUndefined(Undefined):
+    """Raises an UndefinedError if a jinja variable used in a template is not defined"""
+
+    def __str__(self):
+        raise UndefinedError(f"Variable '{self._undefined_name}' is not defined")
+
+
+@pytest.fixture(scope="session")
+def mock_app(tmpdir_factory):
+    # NOTE (felix): We are using the tmpdir_factory rather than tmpdir to allow
+    # this fixture be defined on a session scope.
+
+    # Use the test-settings file for the mocked flask app, but patch the database path
+    os.environ["FLIRROR_SETTINGS"] = os.path.join(FIXTURE_DIR, "test-settings.cfg")
+    config = {"DATABASE_FILE": str(tmpdir_factory.mktemp("data").join("test.db"))}
+
+    # Overwrite the jinja options to make template rendering fail on undefined variables
+    jinja_options = {"undefined": ExceptionUndefined}
+
+    app = create_app(config, jinja_options)
+    app.debug = True
+
+    return app.test_client()
