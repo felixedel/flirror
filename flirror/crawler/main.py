cf39e8a2e85127d3947691af3115383bf6eba3dd
Felix Edel <felixfelix.schmidt@googlemail.com>
2020-03-10 15:03:00 +0100

    Use the Flirror app also in crawler cmd application
    This way we can share the common parts like config loading, database
    creation and module initialization.
    
    We have also full access to the flirror application in case that's
    needed later on.

--- flirror/crawler/main.py
+++ flirror/crawler/main.py
@@ -1,14 +1,11 @@
 import logging
 import os
-from pathlib import Path
 
 import click
-from flask.config import Config
 
-from flirror import FLIRROR_SETTINGS_ENV
+from flirror import create_app
 from flirror.crawler.crawlers import CrawlerFactory
 from flirror.crawler.scheduling import SafeScheduler
-from flirror.database import create_database_and_entities
 from flirror.exceptions import CrawlerConfigError, CrawlerDataError
 
 
@@ -46,33 +43,10 @@ def configure_logger(verbosity):
 def main(ctx, verbosity):
     configure_logger(verbosity)
 
-    # Load the configurations from file
-    config = Config(root_path=".")
-    # TODO Add default settings, once we have some
-    # config.from_object(default_settings)
+    app = create_app()
 
-    if not os.environ.get(FLIRROR_SETTINGS_ENV):
-        raise click.ClickException(
-            "Unable to load configuration. The environment variable 'FLIRROR_SETTINGS' "
-            "is not set. Please set this variable and make it point to a configuration "
-            "file."
-        )
-
-    # Validate that the path exist for a nicer error message.
-    # Otherwise, we will only get a FileNotFoundError() when the config.from_envvar fails.
-    config_path = Path(os.environ[FLIRROR_SETTINGS_ENV])
-    if not config_path.exists():
-        raise click.ClickException(
-            f"The config file '{config_path}' set in 'FLIRROR_SETTINGS' environment "
-            "variable does not exist. Please make this variable point to an existing "
-            "configuration file."
-        )
-
-    config.from_envvar(FLIRROR_SETTINGS_ENV)
-
-    # TODO Validate config?
     # Store everything in click's context object to be available for subcommands
-    ctx.obj = {"config": config}
+    ctx.obj = {"app": app}
 
     if ctx.invoked_subcommand is None:
         ctx.invoke(crawl)
@@ -91,17 +65,12 @@ def main(ctx, verbosity):
 def crawl(ctx, module, periodic):
     LOGGER.info("Hello, Flirror!")
 
-    config = ctx.obj["config"]
-
-    # Connect to the sqlite database
-    db = create_database_and_entities(
-        provider="sqlite", filename=config["DATABASE_FILE"], create_db=True
-    )
+    app = ctx.obj["app"]
 
     # Create the crawler factory to use for initializing new crawlers
     factory = CrawlerFactory()
 
-    config_modules = config.get("MODULES", [])
+    config_modules = app.config.get("MODULES", [])
 
     if module:
         # Filter crawlers for provided module IDs
@@ -142,7 +111,7 @@ def crawl(ctx, module, periodic):
         interval = crawler_config.get("crawler", {}).get("interval")
         crawler = crawler_cls(
             crawler_id=crawler_id,
-            database=db,
+            database=app.extensions["database"],
             **crawler_config["config"],
             interval=interval,
         )
